3a65478674b15d13328b48eaacc26f0e15458156
Ryan Barnes <rybarnes@gmail.com>
2021-07-19 21:55:35 -0400

    new styles

--- gatsby/src/components/Project.js
+++ gatsby/src/components/Project.js
@@ -1,10 +1,68 @@
 import React from 'react'
+import styled from 'styled-components'
+import Img from 'gatsby-image';
+
+const ProjectStyles = styled.div`
+  align-items:center;
+  padding-left: 5%;
+  padding-right: 5%;
+  border: 3px solid #212529;
+  height: 100%;
+
+  .button {
+    background-color: #212529;
+    border-radius: 5px;
+    padding: 15px 15px;
+    margin: 10px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+  }
+
+  a {
+      color: white;
+      text-transform: uppercase;
+      font-weight:600;
+  }
+
+  .image {
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+  }
+
+  h1 {
+      font-size: 3rem;
+  }
+
+`;
 
 export default function Project({ data }) {
     console.log(data)
     return (
-        <div>
-            <h1>{data.title}</h1>
-        </div>
+        <ProjectStyles>
+                <div><h1>{data.title}</h1></div>
+                <div class="image">
+                    <Img fluid={data.image.asset.fluid} alt={data.title} />
+                </div>
+                <p>{data.description}</p>
+                {data.github && <a href={data.github} class="button">See On Github</a>}
+                {data.link && <a href={data.link} class="button">See Live</a>}
+                
+        </ProjectStyles>
     )
 }
+
+/* <ProjectStyles key="{project.id}">
+        <ProjectLeft>
+          <Img fluid={project.image.asset.fluid} />
+        </ProjectLeft>
+        <ProjectRight>
+          <span class="date">{project.date}</span>
+          <a href={project.link}><h1>{project.title}</h1></a>
+          <p>{project.description}</p> 
+          <ul>
+            <li><a href={project.github}>Github</a></li>
+            <li><a href={project.link}>Link</a></li>
+          </ul>
+        </ProjectRight>
+      </ProjectStyles> */
--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -6,77 +6,25 @@ import "@fontsource/inter"
 import Header from "../components/Header";
 import Project from "../components/Project";
 
-const ProjectStyles = styled.div`
-  font-family: 'Inter', sans-serif;
-  background-color:#FEC5BB;
-  margin:50px 50px;
+
+const ProjectGrid = styled.div`
   display: grid;
-  grid-template-columns: 50% auto;
-  align-items: center;
-  .date {
-    font-style: italic;
-    font-size:12px;
-    border-bottom: 2px dashed #FEC5BB;
-
-  }
-  h1{ 
-    font-weight: 200;
-    border-bottom: 5px solid #FEC5BB
-  }
-  text-decoration:none;
-  ul{
-    list-style: none;
-    
-  }
-  ul li {
-    display: block;
-    background-color:#FEC5BB;
-    width: 100px;
-    padding: 10px;
-    margin: 20px;
-    text-align: center;
-  }
-  
-`;
-
-
-const ProjectLeft = styled.div`
-  display:grid;
-  padding:1rem 2rem;
-`;
-
-const ProjectRight = styled.div`
-  align-items:center;
-  padding:1rem 2rem;
-  background-color: white;
-  height: 100%;
-  
+  grid-gap: 4rem;
+  grid-template-columns: repeat(auto-fill, 45%);
 `;
 
 export default function Index({ data }) {
   const projects = data.projects.nodes;
   return (
     <>
-    <Header/>
-    {projects.map((project) => (
-      <>
-      <Project data={project}/>
-      <ProjectStyles key="{project.id}">
-        <ProjectLeft>
-          <Img fluid={project.image.asset.fluid} />
-        </ProjectLeft>
-        <ProjectRight>
-          <span class="date">{project.date}</span>
-          <a href={project.link}><h1>{project.title}</h1></a>
-          <p>{project.description}</p> 
-          <ul>
-            <li><a href={project.github}>Github</a></li>
-            <li><a href={project.link}>Link</a></li>
-          </ul>
-        </ProjectRight>
-      </ProjectStyles>
-      </>
-    ))}
+      <Header/>
+      <ProjectGrid>
+        {projects.map((project) => (
+          <>
+          <Project data={project}/>
+          </>
+        ))}
+      </ProjectGrid>
     </>
     
   )
