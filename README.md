1ecc4a2601e6c5dd6b15d0d424f2e1a49aad18a1
Ryan Barnes <rybarnes@gmail.com>
2021-07-21 20:41:13 -0400

    blog implemented

--- README.md
+++ README.md
@@ -5,6 +5,13 @@ My personal portfolio site, in Gatsby with Sanity backend.
 #### 7.19.2021
 New styles published. Lots still to do: 
 * Implement a simple blog for sharing projects and thoughts.
+- Blog is fuctional, and looks nice enough as very simple tool.
+- Fix the block content style
+- New naviagtion for the blog
+- Eventually generate pages and link to each page. 
+- Add 'see on github' button, or something like that. Codepen or github?
 * Accessibility things to check out, metatags, header details, etc.
 * Better styling of the portfolio cards.
+- Flexible grid, 'stacking' cards.
+- Mobile breakpoints.
 * Spice with some mouse-over animations, etc.
--- gatsby/src/pages/blog.js
+++ gatsby/src/pages/blog.js
@@ -5,8 +5,22 @@ import styled from "styled-components";
 import "@fontsource/inter"
 import Header from "../components/Header";
 import Project from "../components/Project";
+import BlockContent from "@sanity/block-content-to-react";
 
-
+const BlogStyles = styled.div`
+  width: 50%;
+  margin: auto;
+  padding: 2rem;
+  align: center;
+  h2 {
+    font-size:3rem;
+    margin-bottom: 0px;
+  }
+  h3 {
+    font-size:1.5rem;
+    margin-top:0px;
+  }
+`;
 
 
 export default function Index({ data }) {
@@ -15,12 +29,19 @@ export default function Index({ data }) {
   return (
     <>
       <Header/>
-
-      {blogs.map((blog) => (
-          <>
-          <h1>{blog.title}</h1> 
-          </>
-        ))}
+        <BlogStyles>
+        <h2>The Blog</h2>
+        <h3>A place for experiments, works in progress and rambling thoughts.</h3>
+        <hr />
+        {blogs.map((blog) => (
+            <>
+            <h4>{blog.title}</h4> 
+            <Img fluid={blog.image.asset.fluid} alt={blog.title} />
+            <p>Test</p>
+            <BlockContent blocks={blog.text}  />
+            </>
+          ))}
+        </BlogStyles>
     </>
     
   )
@@ -37,6 +58,18 @@ export const query = graphql`
             fluid(maxWidth: 800) {
               ...GatsbySanityImageFluid
             }
+          }
+        }
+        text {
+          _key
+          _type
+          style
+          list
+          _rawChildren
+          children {
+            _key
+            _type
+            text
           }
         }
     }
